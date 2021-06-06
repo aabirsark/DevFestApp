fdd9e3e0c19b8abfd1e30a1a01f95c0c5ddea375
Aabir Sarkar <factbomberz.1@gmail.com>
2021-06-06 11:07:16 +0530

    changes for main.dart only

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+void main() {
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.cyan),
+    );
+  }
+}
